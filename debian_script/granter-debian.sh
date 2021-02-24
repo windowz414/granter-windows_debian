9f9cc9d8b1caaddf0f0dbe9373a6b50b6c5fd55b
Wind/owZ <39265461+windowzytch@users.noreply.github.com>
2021-02-24 05:45:45 +0300

    Debian script: Several touches
    Migrated to Google platform-tools
    Corrected a typo

--- debian_script/granter-debian.sh
+++ debian_script/granter-debian.sh
@@ -1,28 +1,36 @@
-oppocheck=$(adb shell getprop ro.product.manufacturer)
-package=platform-tools
-if ! [ $(id -u) = 0 ]; then
-   echo "Please run this script as root (sudo)"
-   exit 1
-fi
-echo "Installing/updating platform-tools..."
-apt install -y $package
+echo "Pre-script: Getting dependencies... Make sure you have Internet connection."
+sudo apt-get update > /dev/null
+sudo apt-get install wget unzip -y > /dev/null
+echo "Getting latest platform-tools and extracting..."
+cd /tmp
+wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip > /dev/null
+unzip platform-tools-latest-linux.zip > /dev/null
+cd platform-tools-latest-linux
+chmod 777 adb
 clear
 echo "Please connect your phone. Make sure USB debugging is turned on in Developer settings."
-adb wait-for-device
+./adb wait-for-device
 clear
-echo "Checking deive permissions..."
-adb devices
+echo "Checking device permissions..."
+./adb devices
 echo "If you see 'no permissions', please make sure you enabled USB debugging in your device developer settings, set the usb mode to 'File Transfer', and allow the debugging in a prompt on your device, then run the script again. If you don't see such line, press enter."
 read
 echo "Proceeding..."
 echo "Checking Device..."
+oppocheck=$(./adb shell getprop ro.product.manufacturer)
 if echo $oppocheck | grep -q -i "OPPO"; then
     echo "OPPO device found, please make sure you have disabled  permission monitor in developer settings, then press enter. If you don't disable it, Granter will fail."
+    read
 fi
 echo "Granting the shades..."
-adb shell pm grant com.treydev.mns android.permission.WRITE_SECURE_SETTINGS
-adb shell pm grant com.treydev.pns android.permission.WRITE_SECURE_SETTINGS
-adb shell pm grant com.treydev.ons android.permission.WRITE_SECURE_SETTINGS
-adb shell pm grant com.treydev.micontrolcenter android.permission.WRITE_SECURE_SETTINGS
+./adb shell pm grant com.treydev.mns android.permission.WRITE_SECURE_SETTINGS > /dev/null
+./adb shell pm grant com.treydev.pns android.permission.WRITE_SECURE_SETTINGS > /dev/null
+./adb shell pm grant com.treydev.ons android.permission.WRITE_SECURE_SETTINGS > /dev/null
+./adb shell pm grant com.treydev.micontrolcenter android.permission.WRITE_SECURE_SETTINGS > /dev/null
 clear
-echo "All done! You can disconnect your device now."
+echo "All done! You can disconnect your device now. Press enter to exit."
+read
+clear
+echo "Cleaning up..."
+cd ..
+rm -rf platform-tools-latest-linux*
